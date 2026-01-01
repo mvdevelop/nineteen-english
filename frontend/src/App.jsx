b6d2df57bbb6d2637638b246c0894b2ae3816f50
mvdevelop <marcosvmdilly@gmail.com>
2026-01-01 10:46:09 -0300

    feat: refactoring project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,11 @@
 
-import { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
 
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
-import './App.css';
 
 function App() {
-  const [ products, setProducts ] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/produtos');
-        const data = await response.json();
-        setProducts(data);
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
 
   return (
     <>
-      <Navbar />
-      <div className='container'>
-        {Array.isArray(products) && products.map((item, index) => (
-          <div className='card' key={item.id || `product-${index}`}>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img className='img' variant="top" src={`http://localhost:3000/${item.imagem}`} />
-              <Card.Body className='body'>
-                <Card.Title className='name'>{item.nome}</Card.Title>
-                <Card.Text className='description'>{item.descricao}</Card.Text>
-                <button>Content</button>
-              </Card.Body>
-            </Card>
-          </div>
-        ))}
-      </div>
+      
     </>
   );
 }
